8950762246aa1af6c3757212220dc6e1a4fe6610
hnimmagadda1 <103576372+hnimmagadda1@users.noreply.github.com>
2022-11-01 23:28:09 +0530

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,3 +1,3 @@
 def call(){
-  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'git@github.com:hnimmagadda1/Terraform-Test.git']]])
+  checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/hnimmagadda1/Terraform-Test.git']]])
 }
